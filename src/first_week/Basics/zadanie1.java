acd43b55c59cdcc05a82a65417c6c98fe9fd78c2
ceglardo <79514892+ceglardo@users.noreply.github.com>
2021-02-25 18:18:25 +0100

    Zadanie 1

---
+++ src/first_week/Basics/zadanie1.java
@@ -0,0 +1,43 @@
+package first_week.Basics;
+
+public class zadanie1 {
+    public static void main(String[] args) {
+        //zadadnie 1
+        int a = 1;
+        double b = 2.137 ;
+        char c = 'c';
+        String d = "JD=Jest-Dobrze";
+        System.out.println(a+ "\n" +b+ "\n" +c+ "\n" +d);
+
+        System.out.println("\n\n");
+
+        //zadanie 2
+        int suma1 = 10;
+        int suma2 = 10;
+
+        double suma = Math.pow(suma1 , 2) + Math.pow(suma2 , 2) ;
+        System.out.println(suma);
+
+        System.out.println("\n\n");
+
+        //zadanie 3
+
+        int x = 7;
+        double sqrt = Math.sqrt(3);
+        double pot = Math.pow(x , 2);
+
+        double pole = pot * sqrt /4;
+        System.out.println(pole);
+
+        System.out.println("\n\n");
+
+        //zadanie 4
+
+        String Tynoniewiem = "Maciek Cegielny";
+
+        System.out.println(Tynoniewiem);
+
+
+
+    }
+}
