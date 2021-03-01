09f1eff87231b7ea5897d96319551b4ace92193e
ceglardo <79514892+ceglardo@users.noreply.github.com>
2021-03-01 21:24:58 +0100

    poprawki

--- src/secoud_week/zadanie3/zad_dla_chętnych1.java
+++ src/secoud_week/zadanie3/zad_dla_chętnych1.java
@@ -1,18 +1,20 @@
 package secoud_week.zadanie3;
 
 public class zad_dla_chętnych1 {
-    public static void main(String[] args) {
-        int w= 10;
 
-        for(int i = w; i > 0; i--){
+    public static void main(String[] args) {
+
+int w= 10;
+
+for(int i = w; i > 0; i--){
             for(int j = i; j > 0; j--) {
                 System.out.print(" ");
             }
+
             for(int n = 2*(w - i) + 1; n > 0; n--) {
                 System.out.print("*");
             }
             System.out.print("\n");
         }
-
     }
 }
